486ba7890cfbf99d7d51e54e6b77795239f7f8ef
SimbakSmart <simbak.smart@gmail.com>
2023-03-03 22:13:08 -0700

    01 - Body Structure Begin

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+
+  static final String routeName = 'home';
+
+  HomePage({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Text('Home Page'),
+    );
+  }
+}
---
+++ lib/src/pages/setting_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  static final String routeName="settings";
+
+  SettingsPage({required Key key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() {
+    return _SettingsPageState();
+  }
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+
+    );
+  }
+}
